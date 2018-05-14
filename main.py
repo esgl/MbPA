89fdc2e37c48cbbf35edec80238c4b490fa1ee1e
esgl <esgl1987com@163.com>
2018-05-14 18:12:29 +0800

    first play

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def training_knn(model, mnist, task_permutation, use_memory=False):
     last_performance = []
     for task in range(args.num_tasks_to_run):
         logger.info("\nTraining task:{}/{}".format(task + 1, args.num_tasks_to_run))
-        for i in tqdm(range(1000)):
+        for i in tqdm(range(10000)):
             batch = mnist.train.next_batch(args.batch_size)
             batch = (batch[0][:, task_permutation[task]], batch[1])
             if use_memory:
